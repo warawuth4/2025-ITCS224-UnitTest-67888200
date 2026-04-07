12aa25ba1a873e53bfd51c493610d94b0170907f
Warawuth Ngamluea <warawuth.nga@student.mahidol.ac.th>
2026-04-07 08:44:03 +0000

    Refactor age categorization tests for clarity and consistency

--- test_child_age.py
+++ test_child_age.py
@@ -3,13 +3,33 @@ from age import categorize_by_age
 
 class TestIschild(unittest.TestCase):
     
-    def test_child_age(self):
+    def test_is_child(self):
         for age in range(150):
-            print(f"\n{age} is considered as a child.")
-            try:
-                self.assertEqual(categorize_by_age(age), "Child")
-            except:
-                break
+            with self.subTest(age=age):
+                try:
+                    self.assertEqual(categorize_by_age(age), "Child")
+                    print(f"\n{age} is categorized as a Child.")
+                except AssertionError:
+                    continue
+
+    def test_is_adult(self):
+        for age in range(150):
+            with self.subTest(age=age):
+                try:
+                    self.assertEqual(categorize_by_age(age), "Adult")
+                    print(f"\n{age} is categorized as an Adult.")
+                except AssertionError:
+                    continue
+
+    def test_is_golden(self):
+        for age in range(150):
+            with self.subTest(age=age):
+                try:
+                    self.assertEqual(categorize_by_age(age), "Golden")
+                    print(f"\n{age} is categorized as a Golden.")
+                except AssertionError:
+                    continue
+
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=1)
